000dc8c97882685e29a6d249dd052c58c630b2a5
Bogdannb34 <Fidelio6891>
2021-04-28 20:23:44 +0300

    first commit

---
+++ src/main/java/com/plusProject/IQuiz/IQuizApplication.java
@@ -0,0 +1,13 @@
+package com.plusProject.IQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IQuizApplication.class, args);
+	}
+
+}
